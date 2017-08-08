ae8a41e5477ec735490a86e2222790bb86ba72c2
rangerazk <30746997+rangerazk@users.noreply.github.com>
2017-08-08 16:47:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 console.log('Loaded!');
 var imt = document.getElementById('img1');
 var mrg = 0;
-function moveright(){mrg = mrg + 10;
+function moveright(){mrg = mrg + 1;
 imt.style.marginLeft = mrg +'px';}
 imt.onclick = function() {
-    var interval = setInterval(moveright,100);
+    var interval = setInterval(moveright,50);
 };
 
 
