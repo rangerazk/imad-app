d55390ddee856db058d453ba290c653ddbad6310
rangerazk <30746997+rangerazk@users.noreply.github.com>
2017-08-13 22:02:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,18 +7,18 @@ imt.onclick = function() {
     var interval = setInterval(moveright,50);
 };
 
-var button =document.getElementById('bt') ;
+var button = document.getElementById('bt') ;
 
 
 button.onclick = function() {
   
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
-      if (request.readystate == XMLHttpRequest.DONE){
-          if (request.status == 200){
+      if (request.readystate === XMLHttpRequest.DONE){
+          if (request.status === 200){
               var counter = request.responseText;
               var span = document.getElementById('count');
-              span.innerHTML = counter;
+              span.innerHTML = counter.toString();
           }
       }
   };
