7dcbba5921f150b8342d5e3118ba631518f3ba57
rangerazk <30746997+rangerazk@users.noreply.github.com>
2017-08-13 20:16:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ button.onclick = function() {
           if (request.status === 200){
               var counter = request.responseText;
               var span = document.getElementById("count");
-              span.innerhtml = counter.toString;
+              span.innerHTML = counter.toString;
           }
       }
   };
