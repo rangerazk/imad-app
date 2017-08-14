cd2aef9b69d8eb3c2e2f9993e82a7e1028960221
rangerazk <30746997+rangerazk@users.noreply.github.com>
2017-08-14 22:11:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,10 +30,10 @@ button.onclick = function() {
 var nameinput = document.getElementById('name');
 var name = nameinput.value;
 var submit = document.getElementById('submit');
-submit.onClick = function() {
+submit.onclick = function() {
     var names =['name1','name2','name3'];
     var list ='';
-    for (var i=0;i<names.length;i++){
+    for (var i=0;i< names.length;i++){
         list += '<li>' + names[i] + '</li>';
         
     }
