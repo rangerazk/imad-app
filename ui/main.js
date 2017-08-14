715784a34895a3f5ff6cc14a4c5409518e6ebef0
rangerazk <30746997+rangerazk@users.noreply.github.com>
2017-08-14 23:31:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ submit.onclick = function() {
             console.log(request.responseText);
 
     var names =request.responeText;
-    names = JSON.parse(names);
+    names = JSON.Parse(names);
     var list ='';
     for (var i=0;i<names.length;i++){
         list += '<li>'+names[i] +'</li>';
