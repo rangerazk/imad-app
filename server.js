5bec0fc6b069e9ae6eec043bc4889007f4103cf4
rangerazk <30746997+rangerazk@users.noreply.github.com>
2017-09-04 17:37:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,41 @@ var config = {
 }
 var app = express();
 app.use(morgan('combined'));
+function createtemplate(data){
+var title = data.title; 
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+
+var htmltemplate=
+<html>
+<head>
+<title>
+${title}
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+<div class="container">
+<div>
+<a href="/">Home</a>
+</div>
+<hr/>
+<h3>
+${heading}
+</h3>
+<div>
+${date}
+</div>
+<div>
+${content}
+</div>
+</div>
+</body>
+</html>;
+return htmltemplate;
+}
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
