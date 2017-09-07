af8771169057ca8d078ff6099d4da305e54e7c97
rangerazk <30746997+rangerazk@users.noreply.github.com>
2017-09-07 12:31:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,35 +16,10 @@ var title=data.title;
 var date=data.date;
 var heading=data.heading;
 var content=data.content;
-var htmlTemplate= 
-   <html>
-    <head>
-        <title>
-            ${title}
-        </title>
-     <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-       <body>
-       <div class = "container">
-       <div>
-       <a href="/">Home</a>
-       </div>
-        <hr/>
-           <h3>
-        ${heading}
-        </h3>
-        <div>
-        ${date}
-        </div>
-         <div>
-         ${content}
-        </div>
-          </div>
-          </body>
-   </html>
+var htmlTemplate = '<html><head><title>${title}</title><meta name="viewport" content="width=device-width,initial-scale=1"/><link href="/ui/style.css" rel="stylesheet" /></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3>${heading}</h3><div>${date}</div><div>${content}</div></div></body></html>'     
+;
 return htmlTemplate
 ;
-}
 
  
 
